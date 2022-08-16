e1f5f9aa770671c1e14c7a6c49ce3bf884b1c278
taniguchiryuzo <kousei0038@gmail.com>
2022-08-16 17:50:37 +0900

    toppage-create

--- resources/views/hello_world.blade.php
+++ resources/views/hello_world.blade.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Document</title>
 </head>
 <body>
+    <main>
+<!-- <h1 style="">食形態から選ぶ</h1>
+    <p style="color:red">おはようございます</p> -->
+    <section class="title_section">
+         <h1>食形態から選ぶ</h1>
+     </section>
+     <section id=button class="button_section">
+         <button class="button_paste">
+            <img src="./images/rod.png" alt=""> 
+            ペースト
+         </button>
+        <button class="button_mash">
+            <img src="images/heart.png" alt="">
+            マッシュ
+        </button>
+        <button class="button_kizami">
+            <img src="/images/coin.png" alt="">
+            きざみ
+        </button>
+        <button class="button_potage">
+            <img src="/images/program.png" alt="">
+            ポタージュ
+        </button>
+     </section>
+</main>
 
-<h1>食形態</h1>
-    <p style="color:red">おはようございます</p>
 </body>
 </html>
